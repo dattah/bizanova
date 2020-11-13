93c8f19c3ee460b24c743c0188241de992934cfa
joekangudia <joekangudia@gmail.com>
2020-11-13 15:47:23 +0100

    change main directory mytutor to keystagesuccess add information to manifest file

--- keystagesuccess/__manifest__.py
+++ keystagesuccess/__manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "mytutor",
+    'name': "tutormyfuture",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        education website""",
 
     'description': """
-        Long description of module's purpose
+        tutor future
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "bizanova",
+    'website': "http://www.bizanova.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
